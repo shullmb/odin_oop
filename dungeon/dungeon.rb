347bc2b18d129801f7399946da0bd33aa8bd8971
shullmb <dev.shullmb@gmail.com>
2015-09-13 12:36:02 -0700

    add_rooms, malistan

--- dungeon/dungeon.rb
+++ dungeon/dungeon.rb
@@ -8,22 +8,22 @@ class Dungeon
 		@rooms = []
 	end
 
-	class Player
-		attr_accessor :name, :location
-		
-		def initialize(player_name)
-			@name = player_name
-		end
+	def player(name, location)
+
 	end
 
-	class Room
-		attr_accessor :reference, :name, :description, :connections
-		
-		def initialize(reference, name, description, connections)
-			@reference = reference
-			@name = name
-			@description = description
-			@connections = connections
-		end
+	def add_room(reference, name, description, connections)
+		@rooms << Room.new(reference, name, description, connections)
+
 	end
+
+	Player = Struct.new(:name, :location)
+	Room = Struct.new(:reference, :name, :description, :connections)
 end
+
+malistan = Dungeon.new("Charlotte Dogs")
+
+malistan.add_room(:cave_mouth, "Mouth of Cave", "the mouth of a cave", { :south => :field, :north => :first_chamber})
+malistan.add_room(:first_chamber, "First Chamber", "a damp chamber in the cave", {:south => :cave_mouth, :north => :shadow_hall})
+
+
