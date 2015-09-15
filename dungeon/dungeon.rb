7cc23ce72eef08cf47c0c151cf948651e7988fc8
shullmb <dev.shullmb@gmail.com>
2015-09-14 21:21:09 -0700

    tinkering

--- dungeon/dungeon.rb
+++ dungeon/dungeon.rb
@@ -6,7 +6,6 @@ class Dungeon
 	def initialize(player_name)
 		@player = Player.new(player_name)
 		@rooms = []
-		@directions = []
 	end
 
 	def start(location)
@@ -34,7 +33,6 @@ class Dungeon
 
 	def add_room(reference, name, description, connections)
 		@rooms << Room.new(reference, name, description, connections)
-		@directions << reference
 	end
 
 	class Player
@@ -72,7 +70,22 @@ class Dungeon
 		end
 
 		def full_description
-			@name + "\n\nYou are in #{@description}.\n"
+			puts @name + "\n\nYou are in #{@description}.\n\n"
+
+			puts @connections.keys.to_s #maybe comment out when built completely 
+		end
+	end
+
+	class Monster
+		attr_accessor :name, :hp
+
+		def initialize(name, hp)
+			@name = name
+			@hp = hp
+		end
+
+		def attack(damage)
+			#CODE ME
 		end
 	end
 
@@ -87,6 +100,8 @@ malistan.add_room(:cave_mouth, "Mouth of Cave", "the mouth of a cave", { :south 
 malistan.add_room(:first_chamber, "First Chamber", "a damp chamber in the cave", {:south => :cave_mouth, :north => :shadow_hall})
 malistan.add_room(:shadow_hall,"Shadow Hall", "a narrow hall. You can only go forward... or turn back", {:north => :darkness, :south => :first_chamber})
 malistan.add_room(:darkness, "Darkness", "a very dark place. You can feel a slight draft and can tell that there is a fork in your path. you can push forward in a northerly direction or cut east", {:north => :stairway, :east => :deathpit, :south => :shadow_hall})
+malistan.add_room(:stairway, "The Winding stairway", "winding staircase. You can see the faint flicker of a candle just around the bend... but you can never seem to catch it.",{:north => :empty_chamber, :south => :darkness})
+malistan.add_room(:empty_chamber, "the Empty Chamber", "an empty room. A single candle burns on the mantle of a long cold fireplace. There is a door in every direction. Things may start to get interesting", {:south => :stairway, :north => :cave_mouth, :west => :sweet_tooth, :east => :grinder})
 malistan.add_room(:deathpit, "Deathpit", "a dark place. The draft is stronger... heavier... hotter... smells of rotting meat. Turn back before it is too la--",{:resurrect => :cave_mouth})
 #Start
 
@@ -113,9 +128,12 @@ loop do
 	if malistan.find_room_in_direction(direction).to_s != ""
 
 		malistan.go(direction.to_sym)
+
 	elsif direction == :exit
+		
 		puts "Exiting"
 		break
+
 	else
 		puts "I'm sorry. That is not a valid command."
 		next
