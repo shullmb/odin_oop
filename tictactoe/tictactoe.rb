ff4ebddedc0310b2b791da7180969ebd29c9c241
shullmb <dev.shullmb@gmail.com>
2015-09-19 09:58:17 -0700

    added players

--- tictactoe/tictactoe.rb
+++ tictactoe/tictactoe.rb
@@ -9,11 +9,25 @@ class Board
 	end	
 
 	def start
+		puts "Welcome!"
+		puts "Player 1, please choose a name: "
+		name_1 = gets.chomp.to_s
+		puts "and a symbol: "
+		sym_1 = gets.chomp.upcase
+		@player1 = Player.new(name_1, sym_1)
+		puts "Player 2, please choose a name:"
+		name_2 = gets.chomp.upcase
+		puts "and a symbol: "
+		sym_2 = gets.chomp.upcase
+		@player2 = Player.new(name_2, sym_2)
+
 		display
 	end
 
 	def display
 		puts %Q{
+#{player1}: #{sym_1}
+#{player2}: #{sym_2}
 		 --- --- ---
 		| #{grid_1[0]} | #{grid_1[1]} | #{grid_1[2]} | 1
 		 --- --- ---
@@ -24,15 +38,26 @@ class Board
 		  A   B   C
  		}
 	end
+
+	def victory
+		#add comparison cases
+		puts "#{player1} is victorious!"
+	end
 	
 	class Player
 		attr_accessor :name
-		def initialize(name)
+		attr_reader :sym
+		def initialize(name, sym)
 			@name = name
+			@sym = sym
 		end
 		def turn
 			#prompt player for selection
 
 		end
 	end
+end
+
+until victory do 
+	
 end
