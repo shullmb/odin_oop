095f28f8235d59260a06b0345cc888e3ac57a78e
shullmb <dev.shullmb@gmail.com>
2015-09-20 08:05:20 -0700

    added case statement and turn selection validation

--- tictactoe/tictactoe.rb
+++ tictactoe/tictactoe.rb
@@ -10,43 +10,85 @@ class Board
 
 	def start
 		puts "Welcome!"
-		# print "Player 1, please choose a name: "
-		# name_1 = gets.chomp.to_s
-		# print "and a symbol: "
-		# sym_1 = gets.chomp.upcase
-		# @player1 = Player.new(name_1, sym_1[0])
-		# print "Player 2, please choose a name: "
-		# name_2 = gets.chomp.to_s
-		# print "and a symbol: "
-		# sym_2 = gets.chomp.upcase
-		# @player2 = Player.new(name_2, sym_2[0])
-		players = ["Player 1", "Player 2"]
-		players.each {prompt}
+		print "Player 1, please choose a name: "
+		name_1 = gets.chomp.to_s
+		print "and a symbol: "
+		sym_1 = gets.chomp.upcase
+		@player1 = Player.new(name_1, sym_1[0])
+		print "Player 2, please choose a name: "
+		name_2 = gets.chomp.to_s
+		print "and a symbol: "
+		sym_2 = gets.chomp.upcase
+		@player2 = Player.new(name_2, sym_2[0])
 
 		display
 	end
 
-	def prompt
-		print "Please choose a name: "
-		name = gets.chomp.to_s
-		print "and a symbol: "
-		sym = gets.chomp.upcase
-		@player = Player.new(name, sym[0])
-	end
 	#need to break start up into multiple methods and add validation
+
+	# def prompt
+	# 	print "Please choose a name: "
+	# 	name = gets.chomp.to_s
+	# 	print "and a symbol: "
+	# 	sym = gets.chomp.upcase
+	# 	@player = Player.new(name, sym[0])
+	# end
+	
+	def turn
+		#get grid coordinate
+		print " select your coordinate: "
+		move = gets.chomp.upcase
+		execute_turn(move)
+
+		display
+	end
+	
+	def execute_turn(move)
+		case move[0]
+		when "A"
+			case move[1]
+			when 1
+				grid_1[0] = @sym
+			when 2
+				grid_1[1] = @sym
+			when 3
+				grid_1[2] = @sym
+			end
+		when "B"
+			case move[1]
+			when 1
+				grid_2[0] = @sym
+			when 2
+				grid_2[1] = @sym
+			when 3
+				grid_2[2] = @sym
+			end
+		when "C"
+			case move[1]
+			when 1
+				grid_3[0] = @sym
+			when 2
+				grid_3[1] = @sym
+			when 3
+				grid_3[2] = @sym
+			end
+		else 
+			puts "that is not a valid selection"
+		end
+	end
 
 	def display
 		puts %Q{
 #{@player1.name}: #{@player1.sym}
 #{@player2.name}: #{@player2.sym}
 		 --- --- ---
-		| #{grid_1[0]} | #{grid_1[1]} | #{grid_1[2]} | 1
+		| #{grid_1[0]} | #{grid_1[1]} | #{grid_1[2]} | A
 		 --- --- ---
-		| #{grid_2[0]} | #{grid_2[1]} | #{grid_2[2]} | 2
+		| #{grid_2[0]} | #{grid_2[1]} | #{grid_2[2]} | B
 		 --- --- ---			
-		| #{grid_3[0]} | #{grid_3[1]} | #{grid_3[2]} | 3
+		| #{grid_3[0]} | #{grid_3[1]} | #{grid_3[2]} | C
 		 --- --- ---
-		  A   B   C
+		  1   2   3
  		}
 	end
 
@@ -62,13 +104,5 @@ class Board
 			@name = name
 			@sym = sym
 		end
-		def turn
-			#prompt player for selection
-
-		end
 	end
 end
-
-tictactoe = Board.new
-
-tictactoe.start
