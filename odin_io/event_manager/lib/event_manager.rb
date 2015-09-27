0bc2fdcdf5e497b91d602a04b7701d3955b3ff1a
shullmb <dev.shullmb@gmail.com>
2015-09-27 09:04:34 -0700

    working with ERB lib

--- odin_io/event_manager/lib/event_manager.rb
+++ odin_io/event_manager/lib/event_manager.rb
@@ -2,6 +2,7 @@
 
 require 'csv'
 require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -11,23 +12,22 @@ end
 
 def legislators_by_zipcode(zipcode)
 	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-	
-	legislators_name = legislators.collect do |legislator|
-		"#{legislator.first_name} #{legislator.last_name}"
-	end
-
-	legislators_name.join(", ")
 end
 
 puts "Event Manager initialized"
 
 contents = CSV.open "../event_attendees.csv", headers: true, header_converters: :symbol
+
+template_letter = File.read "../form_letter.html.erb"
+erb_template = ERB.new template_letter
+
 contents.each do |row|
 	name = row[:first_name]
-
 	zipcode = clean_zipcode(row[:zipcode])		
 	
 	legislators = legislators_by_zipcode(zipcode)
 
-	puts "#{name} #{zipcode} #{legislators}"
+	form_letter = erb_template.result(binding)
+
+	puts form_letter
 end
