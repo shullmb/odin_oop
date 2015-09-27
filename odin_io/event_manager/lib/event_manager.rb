8500740d011b707bbac905901aa42e1f9630a6dc
shullmb <dev.shullmb@gmail.com>
2015-09-27 08:21:04 -0700

    tutorial: working with CSV lib

---
+++ odin_io/event_manager/lib/event_manager.rb
@@ -0,0 +1,33 @@
+#EVENT MANAGER
+
+require 'csv'
+require 'sunlight/congress'
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+def clean_zipcode(zipcode)
+	zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+	
+	legislators_name = legislators.collect do |legislator|
+		"#{legislator.first_name} #{legislator.last_name}"
+	end
+
+	legislators_name.join(", ")
+end
+
+puts "Event Manager initialized"
+
+contents = CSV.open "../event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+	name = row[:first_name]
+
+	zipcode = clean_zipcode(row[:zipcode])		
+	
+	legislators = legislators_by_zipcode(zipcode)
+
+	puts "#{name} #{zipcode} #{legislators}"
+end
