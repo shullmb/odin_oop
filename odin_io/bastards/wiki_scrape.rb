01f69e8a7d2761a2be920beafc5140c75963bb0b
shullmb <dev.shullmb@gmail.com>
2015-09-28 06:18:57 -0700

    Bastard Book of Ruby

---
+++ odin_io/bastards/wiki_scrape.rb
@@ -0,0 +1,11 @@
+#Scrape Frontpage of Wikipedia
+
+require 'rubygems'
+require 'rest-client'
+
+wiki_url = "http://en.wikipedia.org/"
+wiki_local_file = "wikipage.html"
+
+File.open(wiki_local_file, "w") do |file|
+	file.write(RestClient.get(wiki_url))
+end
