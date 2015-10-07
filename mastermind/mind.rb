cd577a322b4e063705b69f37784bfa62be751e81
shullmb <dev.shullmb@gmail.com>
2015-10-06 18:03:47 -0700

    set pattern, display, etc...

--- mastermind/mind.rb
+++ mastermind/mind.rb
@@ -2,14 +2,35 @@ module Mastermind
 	class Board
 		attr_accessor :pattern, :a, :b, :c, :d
 		@@options = ["R","B","Y","W"]
+		@@guess = 0
 		
 		def initialize
 			@pattern = {a: " ", b: " ", c: " ", d: " "}
+			set_pattern
 		end
 
+		def display
+			puts "\n\n\n"
+			puts "|| #{self.pattern[:a]} || #{self.pattern[:b]} || #{self.pattern[:c]} || #{self.pattern[:d]} ||".center(100)
+			puts "\n\n\n\n"
+			puts "Guesses: #{@@guess}".center(100)
+			
+		end
+
+		def prompt
+			print "Proposed pattern: "
+			@proposed = gets.chomp.upcase
+		end
+
+		def compare
+			
+		end
+
+		private
 		def set_pattern
-			self.pattern.each do |k|
-				@pattern[k.to_sym] = @@options.sample
+			self.pattern.each do |key, value|
+				value = @@options.sample
+				self.pattern[key] = value
 			end 
 		end
 	end
